00ec35297610ba5d9d0c4ede559d18d18e34ef3d
davidchen0970 <apple1051629@gmail.com>
2026-08-28 17:58:46 +0800

    feat: add debug build option via meson_options

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main() {
+    #ifdef DEBUG_MODE
+    std::cout << "[Debug] 偵錯模式已開啟！" << std::endl;
+    #endif
+
     std::cout << "Hello, Meson!" << std::endl;
     return 0;
 }
